893d27398ce9f6e8a295c782f7e0076f515d7671
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-01-13 16:56:22 +0400

    Add files via upload

---
+++ ConnSql/ConnSql/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ConnSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbGroupType = new System.Windows.Forms.ComboBox();
+            this.cmbGroup = new System.Windows.Forms.ComboBox();
+            this.txtStuName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbGroupType
+            // 
+            this.cmbGroupType.FormattingEnabled = true;
+            this.cmbGroupType.Location = new System.Drawing.Point(351, 66);
+            this.cmbGroupType.Name = "cmbGroupType";
+            this.cmbGroupType.Size = new System.Drawing.Size(158, 28);
+            this.cmbGroupType.TabIndex = 0;
+            this.cmbGroupType.SelectedIndexChanged += new System.EventHandler(this.cmbGroupType_SelectedIndexChanged);
+            // 
+            // cmbGroup
+            // 
+            this.cmbGroup.FormattingEnabled = true;
+            this.cmbGroup.Location = new System.Drawing.Point(351, 135);
+            this.cmbGroup.Name = "cmbGroup";
+            this.cmbGroup.Size = new System.Drawing.Size(152, 28);
+            this.cmbGroup.TabIndex = 1;
+            // 
+            // txtStuName
+            // 
+            this.txtStuName.Location = new System.Drawing.Point(85, 96);
+            this.txtStuName.Name = "txtStuName";
+            this.txtStuName.Size = new System.Drawing.Size(168, 26);
+            this.txtStuName.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(619, 98);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(129, 52);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgv.Location = new System.Drawing.Point(0, 347);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 62;
+            this.dgv.RowTemplate.Height = 28;
+            this.dgv.Size = new System.Drawing.Size(913, 227);
+            this.dgv.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 574);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtStuName);
+            this.Controls.Add(this.cmbGroup);
+            this.Controls.Add(this.cmbGroupType);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbGroupType;
+        private System.Windows.Forms.ComboBox cmbGroup;
+        private System.Windows.Forms.TextBox txtStuName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ ConnSql/ConnSql/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnSql
+{
+    public partial class Form1 : Form
+    {
+        private string connectionStr;
+        public Form1()
+        {
+            connectionStr = ConfigurationManager.ConnectionStrings["P213Db"].ConnectionString;
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            #region Scala   
+            //using(SqlConnection conn = new SqlConnection(connectionStr))
+            //{
+            //    conn.Open();
+            //    string query = "Select Name from Students where Id=1";
+            //    using(SqlCommand comm = new SqlCommand(query, conn))
+            //    {
+            //        object result =await comm.ExecuteScalarAsync();
+            //        MessageBox.Show(result.ToString());
+            //    }
+            //}
+            #endregion
+
+            string queryList = "Select Name from Types";
+            GetData(queryList, cmbGroupType);
+
+            string dgvQuery = "select * from StuDeteail";
+            using(SqlConnection conn=new SqlConnection(connectionStr))
+            {
+                conn.Open();
+                using(SqlCommand comm=new SqlCommand(dgvQuery,conn))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(comm);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dgv.DataSource = table;
+                }
+            }
+        }
+
+        private async void cmbGroupType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbGroup.Items.Clear();
+            cmbGroup.Text = "";
+            string type = cmbGroupType.Text.Trim();
+            string query = "Select Name from Groups" +
+                    " where TypeId = " +
+                    $"(select Id from Types where Name = '{type}')";
+            GetData(query, cmbGroup);
+        }
+
+        private async void GetData(string query,ComboBox cmb)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                conn.Open();
+                using (SqlCommand comm = new SqlCommand(query, conn))
+                {
+                    SqlDataReader reader = await comm.ExecuteReaderAsync();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            cmb.Items.Add(reader.GetValue(0));
+                        }
+                    }
+                }
+            }
+        }
+
+        private async void btnAdd_Click(object sender, EventArgs e)
+        {
+            string name = txtStuName.Text.Trim();
+            string group = cmbGroup.Text.Trim();
+
+            if (name == "" || group == "")
+            {
+                MessageBox.Show("Fill all input", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string query = "insert into Students values" +
+                $"('{name}', (select Id from Groups where Name = '{group}'))";
+            using(SqlConnection conn=new SqlConnection(connectionStr))
+            {
+                conn.Open();
+                using(SqlCommand comm=new SqlCommand(query, conn))
+                {
+                    int result =await comm.ExecuteNonQueryAsync();
+                    if (result != 0)
+                    {
+                        MessageBox.Show("Successfully added");
+                    }
+                }
+            }
+
+        }
+    }
+}
